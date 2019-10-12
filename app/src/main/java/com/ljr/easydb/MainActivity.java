37b4e1e635bafa3265cdc7fcc36afd456b7752c5
ljrRookie <15622732935@163.com>
2019-10-12 18:04:31 +0800

    添加更新，查询，删除操作

--- app/src/main/java/com/ljr/easydb/MainActivity.java
+++ app/src/main/java/com/ljr/easydb/MainActivity.java
@@ -2,23 +2,74 @@ package com.ljr.easydb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.ljr.easydb.bean.table.User;
 import com.ljr.easydb.db.BaseDao;
 import com.ljr.easydb.db.BaseDaoFactory;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "EASY_DB >>>>> ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //插入数据
         findViewById(R.id.btn_insert).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 BaseDao<User> baseDao = BaseDaoFactory.getInstance().getBaseDao(User.class);
-                long ljr = baseDao.insert(new User(12, "ljr", "123456"));
+                baseDao.insert(new User(12, "ljr", "123456"));
+                baseDao.insert(new User(1, "lzq", "6547521"));
+                baseDao.insert(new User(3, "lzq", "634521"));
+                baseDao.insert(new User(24, "lzq", "654321"));
+                baseDao.insert(new User(65, "lzq", "65457884321"));
+                baseDao.insert(new User(2, "lzq", "65445454321"));
+                baseDao.insert(new User(7, "lzq", "21212"));
+                baseDao.insert(new User(5, "林佳荣", "999999"));
+                baseDao.insert(new User(60, "林子淇", "000000"));
+            }
+        });
+
+        //更新数据
+        findViewById(R.id.btn_update).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update TB_USER where name='ljr' set password='ljr123'
+                BaseDao<User> baseDao = BaseDaoFactory.getInstance().getBaseDao(User.class);
+                User user = new User(5, "林佳豪", "555333");
+                User where = new User();
+                where.setId(5);
+                baseDao.update(user, where);
+            }
+        });
+        //删除数据
+        findViewById(R.id.btn_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BaseDao<User> baseDao = BaseDaoFactory.getInstance().getBaseDao(User.class);
+                User where = new User();
+                where.setName("ljr");
+                baseDao.delete(where);
+
+
+            }
+        });
+
+        //查询数据
+        findViewById(R.id.btn_select).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BaseDao<User> baseDao = BaseDaoFactory.getInstance().getBaseDao(User.class);
+                User where = new User();
+                where.setName("lzq");
+                List<User> query = baseDao.query(where);
+                for (int i = 0; i < query.size(); i++) {
+                    Log.e(TAG," 数据库数据 第"+i+"条: "+ query.get(i).toString());
+                }
             }
         });
     }
--- app/src/main/java/com/ljr/easydb/db/BaseDao.java
+++ app/src/main/java/com/ljr/easydb/db/BaseDao.java
@@ -9,8 +9,10 @@ import com.ljr.easydb.annotation.DbField;
 import com.ljr.easydb.annotation.DbTable;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -24,7 +26,7 @@ public class BaseDao<T> implements IBaseDao<T> {
     private Class<T> mEntityClass;
     //标识，用来标识是否已经做过初始化
     private boolean mIsInit = false;
-    //定义一个缓存空间（key 字段名  value 成员变量）
+    //定义一个缓存空间（key 字段名: "USER_ID"  value 成员变量: "private java.lang.Integer com.ljr.easydb.bean.table.User.id"）
     private HashMap<String, Field> mCacheMap;
 
     /**
@@ -133,12 +135,109 @@ public class BaseDao<T> implements IBaseDao<T> {
         return stringBuilder.toString();
     }
 
+    /**
+     * 插入数据
+     * @param entity 插入实体
+     * @return
+     */
     @Override
     public long insert(T entity) {
         // user 对象 转换为contentvalues  new User(id = 1,name = "ljr",password = "password");
         Map<String,String> map = getValues(entity);
         ContentValues values = getContentValues(map);
         return mSQLiteDatabase.insert(mTableName,null,values);
+    }
+
+    /**
+     * 更新数据
+     * @param entity 修改实体
+     * @param where 搜索实体
+     * @return
+     */
+    @Override
+    public long update(T entity, T where) {
+        Map map = getValues(entity);
+        ContentValues values = getContentValues(map);
+        Map whereMap = getValues(where);
+        Condition condition = new Condition(whereMap);
+        return mSQLiteDatabase.update( mTableName,values,condition.whereCause,condition.whereArgs);
+    }
+
+    /**
+     * 删除数据
+     * @param where
+     * @return
+     */
+    @Override
+    public long delete(T where) {
+        Map map = getValues(where);
+        Condition condition = new Condition(map);
+        return mSQLiteDatabase.delete(mTableName,condition.whereCause,condition.whereArgs);
+    }
+
+    @Override
+    public List<T> query(T where) {
+        return query(where,null,null,null);
+    }
+
+    @Override
+    public List<T> query(T where, String orderBy, Integer startIndex, Integer limit) {
+        Map map = getValues(where);
+        // select * from tableName  limit 0,10;
+        String limitString = null;
+        //String seclections = "where 1=1 and id=? and name=?";
+        //String selectionArgs = String[]{,""};
+        //select * from tableName where id=? and name=?
+        if(startIndex != null && limit != null){
+            limitString = startIndex+" , "+limit;
+        }
+        Condition condition = new Condition(map);
+        Cursor cursor = mSQLiteDatabase.query(mTableName,null,condition.whereCause,condition.whereArgs,null,null,
+                orderBy,limitString);
+        // 定义一个解析游标的方法
+        List<T> result = getResult(cursor,where);
+        return result;
+    }
+    private List<T> getResult(Cursor cursor, T obj) {
+        ArrayList list = new ArrayList();
+        Object item = null;// User user = null;
+        while (cursor.moveToNext()){
+            try {
+                item = obj.getClass().newInstance(); // user = new User(); user.setId(cursor.getId);
+                Iterator iterator = mCacheMap.entrySet().iterator();// 成员变量
+                while (iterator.hasNext()){
+                    Map.Entry entry = (Map.Entry)iterator.next();
+                    // 获取列名
+                    String columnName = (String)entry.getKey();
+                    // 以列名拿到列名在游标中的位置
+                    int columnIndex = cursor.getColumnIndex(columnName);
+                    // 获取成员变量的类型
+                    Field field = (Field) entry.getValue();
+                    Class type = field.getType();
+                    // cursor.getString(columnIndex);
+                    if(columnIndex != -1){
+                        if(type == String.class){
+                            // User user = new User
+                            // user.setId(1);  id.set(user,1);
+                            field.set(item,cursor.getString(columnIndex));
+                        }else if(type== Double.class){
+                            field.set(item,cursor.getDouble(columnIndex));
+                        }else if(type== Integer.class){
+                            field.set(item,cursor.getInt(columnIndex));
+                        }else if(type== Long.class){
+                            field.set(item,cursor.getLong(columnIndex));
+                        }else if(type==byte[].class){
+                            field.set(item,cursor.getBlob(columnIndex));
+                        }
+                    }
+                }
+                list.add(item);
+            } catch (IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        cursor.close();
+        return list;
     }
 
     private ContentValues getContentValues(Map<String, String> map) {
@@ -184,4 +283,32 @@ public class BaseDao<T> implements IBaseDao<T> {
         }
         return map;
     }
+
+
+
+
+    private class Condition{
+        private String whereCause;
+        private String[] whereArgs;
+
+          public Condition(Map<String,String> whereMap){
+            ArrayList list = new ArrayList();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("1=1");
+            // 获取所有的字段名
+            Set keys = whereMap.keySet();
+            Iterator iterator = keys.iterator();
+            while (iterator.hasNext()){
+                String key = (String)iterator.next();
+                String value = whereMap.get(key);
+                if(value != null){
+                    stringBuilder.append(" and ").append(key).append(" =?");
+                    list.add(value);
+                }
+            }
+            this.whereCause = stringBuilder.toString();
+            this.whereArgs = (String[])list.toArray(new String[list.size()]);
+        }
+    }
+
 }
